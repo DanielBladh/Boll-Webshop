9c930fdb4dbd39c58aba234b157e2f2046d2e474
Lina Andersson <lina.andersson@student.kyh.se>
2022-01-14 23:59:24 +0100

    product fix

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 const articles = JSON.parse(localStorage.bollar);
+
+
+// LINA //
+let exampleModal = document.getElementById('exampleModal')
+exampleModal.addEventListener('show.bs.modal', function (event) {
+    let product = event.relatedTarget
+    let productid = product.getAttribute('data-product-id'); //rad 56
+    products = JSON.parse(localStorage.getItem('bollar')); // från T´s lista
+
+    products.forEach(product => {
+        if (product.productId == productid) {
+            console.log(product.title);
+            setProductData("title", product)
+            setProductData("price", product)
+            setProductData("extraEquipment", product)
+            setProductData("material", product)
+            setProductData("color", product)
+            setProductData("level", product)
+        }
+
+    });
+})
+function setProductData(value, product) {
+    document.getElementById(value).innerHTML = product[value]
+}
